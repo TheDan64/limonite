cd3dc162578a6bd78ea77c37f8ffed151484331c
TheDan64 <ThaDan64@gmail.com>
2014-07-12 12:22:35 -0400

    Edited first test. Switched tabs to spaces.

--- src/syntax/core/keywords.rs
+++ src/syntax/core/keywords.rs
@@ -6,62 +6,62 @@
 use std::from_str::FromStr;
 
 pub enum Keyword {
-	And,
-	Or,
-	Not,
-	Is,
-	Class,
-	Fn,
-	Var,
-	Def,
-	Assert,
-	Else,
-	If,
-	For,
-	While,
-	Return,
-	Equals,
-	Use,
-	From,
-	As,
-	When,
-	Throws,
-	Break,
-	Continue,
-	Pass,
-	
-	// Print is temporaily a statement until we have functions
-	Print 
+    And,
+    Or,
+    Not,
+    Is,
+    Class,
+    Fn,
+    Var,
+    Def,
+    Assert,
+    Else,
+    If,
+    For,
+    While,
+    Return,
+    Equals,
+    Use,
+    From,
+    As,
+    When,
+    Throws,
+    Break,
+    Continue,
+    Pass,
+    
+    // Print is temporaily a statement until we have functions
+    Print 
 }
 
 impl FromStr for Keyword {
-	fn from_str(s: &str) -> Option<Keyword> {
-		match s {
-			"and"      => Some(And),
-			"or"       => Some(Or),
-			"not"      => Some(Not),
-			"is"       => Some(Is),
-			"class"    => Some(Class),
-			"fn"       => Some(Fn),
-			"var"      => Some(Var),
-			"def"      => Some(Def),
-			"assert"   => Some(Assert),
-			"else"     => Some(Else),
-			"if"       => Some(If),
-			"for"      => Some(For),
-			"while"    => Some(While),
-			"return"   => Some(Return),
-			"equals"   => Some(Equals),
-			"use"      => Some(Use),
-			"from"     => Some(From),
-			"as"       => Some(As),
-			"when"     => Some(When),
-			"throws"   => Some(Throws),
-			"break"    => Some(Break),
-			"continue" => Some(Continue),
-			"pass"     => Some(Pass),
-			"print"    => Some(Print),
-			_          => None
-		}
-	}
+    fn from_str(s: &str) -> Option<Keyword> {
+        match s {
+            "and"      => Some(And),
+            "or"       => Some(Or),
+            "not"      => Some(Not),
+            "is"       => Some(Is),
+            "class"    => Some(Class),
+            "fn"       => Some(Fn),
+            "var"      => Some(Var),
+            "def"      => Some(Def),
+            "assert"   => Some(Assert),
+            "else"     => Some(Else),
+            "if"       => Some(If),
+            "for"      => Some(For),
+            "while"    => Some(While),
+            "return"   => Some(Return),
+            "equals"   => Some(Equals),
+            "use"      => Some(Use),
+            "from"     => Some(From),
+            "as"       => Some(As),
+            "when"     => Some(When),
+            "throws"   => Some(Throws),
+            "break"    => Some(Break),
+            "continue" => Some(Continue),
+            "pass"     => Some(Pass),
+            "print"    => Some(Print),
+            _          => None
+        }
+    }
 }
--- src/syntax/lexer.rs
+++ src/syntax/lexer.rs
@@ -4,33 +4,33 @@
 extern crate keywords;
 
 pub enum Token {
-	// True, False
-	BooleanLiteral(bool),
+    // True, False
+    BooleanLiteral(bool),
 
-	// Variables, fn names
-	Identifier(String),
+    // Variables, fn names
+    Identifier(String),
 
-	// Reserved words
-	Keyword(keywords::Keyword),
+    // Reserved words
+    Keyword(keywords::Keyword),
 
-	// >> Comments. Eventually multiline as well
-	Comment(String),
+    // >> Comments. Eventually multiline as well
+    Comment(String),
 
-	// End of file
-	EOF
+    // End of file
+    EOF
 }
 
 
 
 pub struct Lexer {
-	pub tokens : Vec<Token>,
-	line_number : uint,
-	column_number : uint
+    pub tokens : Vec<Token>,
+    line_number : uint,
+    column_number : uint
 }
 
 
 
 // Temporary main
 fn main() {
-	
+    
 }
