60ef70f5da632ae5e4c519724231f4df01c3f0e2
Daniel Kolsoi <thadan64@gmail.com>
2016-01-26 22:41:38 -0500

    Moved bulk type checker code to own method
    Added display for Literals

--- src/semantic/type_checker.rs
+++ src/semantic/type_checker.rs
@@ -1,3 +1,6 @@
+use std::str;
+
+use lexical::types::Types;
 use syntax::expr::ExprWrapper;
 use syntax::expr::Expr::*;
 use semantic::analyzer_trait::ASTAnalyzer;
@@ -8,11 +11,8 @@ impl TypeChecker {
     pub fn new() -> TypeChecker {
         TypeChecker
     }
-}
 
-impl ASTAnalyzer for TypeChecker {
-    fn analyze(&mut self, ast_root: &mut ExprWrapper) {
-        // TODO: Handle errors, Option<ExprWrapper> like elsewhere for now?
+    fn analyze_to_type(&mut self, ast_root: &mut ExprWrapper) -> Option<String> {
         match ast_root.get_mut_expr() {
             &mut Block(ref mut vec) => {
                 for expr_wrapper in vec {
@@ -21,22 +21,55 @@ impl ASTAnalyzer for TypeChecker {
                 }
             },
             &mut VarDecl(ref mut const_, ref mut name, ref mut type_, ref mut expr_wrapper) => {
-                let expr_type = expr_wrapper.get_expr();
+                let rhs_type = self.analyze_to_type(expr_wrapper);
 
-                // assert!(expr_type == Literal | FnCall | )
+                println!("Var decl rhs type: {:?}", rhs_type);
+
+                // How to get Expr's type?
+                // Basics:
+                // Literal -> Type
+                // Variable -> VarName -> Lookup VarDecl -> Type
+                // FnCall -> FnName -> LookUp FnDecl -> Type
+                // InfixOp -> analyze bubble up type from Expr op Expr
+                // UnaryOp -> analyze bubble up type from Expr
+
+                // Future?:
+                // if rust style let i = if a {} else {}
+                // If needs lookup all returns in all possible code blocks, assert they're the same type
+
+
+
 
                 match type_ {
-                    &mut Some(ref t) => {
-                        if *t != String::new() {
-                            panic!("Error goes here")
+                    &mut Some(ref str) => {
+                        match str.parse::<Types>() {
+                            Ok(t) => {
+                                if t != Types::Bool { // FIXME
+                                    panic!("Error goes here");
+                                }
+                            },
+                            Err(()) => {
+                                // TODO: Custom type
+                                panic!("Found a custom type reference");
+                            }
                         }
                     },
-                    &mut None => *type_ = Some(String::new())
+                    &mut None => *type_ = Some("i32".parse().unwrap()) // FIXME
                 }
 
             },
-            &mut FnCall(ref name, ref args) => (),
+            &mut FnCall(ref name, ref args) => (), // FIXME
+            &mut Literal(ref literal) => return Some(literal.to_string()), // Done?
             node => panic!("Unimplemented node: {:?}", node)
         }
+
+        None // tmp
+    }
+}
+
+impl ASTAnalyzer for TypeChecker {
+    fn analyze(&mut self, ast_root: &mut ExprWrapper) {
+        // TODO: Handle errors, Option<ExprWrapper> like elsewhere for now?
+        self.analyze_to_type(ast_root);
     }
 }
--- src/syntax/literals.rs
+++ src/syntax/literals.rs
@@ -1,13 +1,32 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq)]
 pub enum Literals {
-	UTF8String(String),
-	UTF8Char(char),
-	I32Num(i32),
-	I64Num(i64),
-	U32Num(u32),
-	U64Num(u64),
-	F32Num(f32),
-	F64Num(f64),
-	Bool(bool),
-	_None
+    UTF8String(String),
+    UTF8Char(char),
+    I32Num(i32),
+    I64Num(i64),
+    U32Num(u32),
+    U64Num(u64),
+    F32Num(f32),
+    F64Num(f64),
+    Bool(bool),
+    _None
+}
+
+impl fmt::Display for Literals {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match *self {
+            Literals::UTF8String(_) => "str",
+            Literals::UTF8Char(_) => "char",
+            Literals::I32Num(_) => "i32",
+            Literals::I64Num(_) => "i64",
+            Literals::U32Num(_) => "u32",
+            Literals::U64Num(_) => "u64",
+            Literals::F32Num(_) => "f32",
+            Literals::F64Num(_) => "f64",
+            Literals::Bool(_) => "bool",
+            Literals::_None => "None"
+        })
+    }
 }
